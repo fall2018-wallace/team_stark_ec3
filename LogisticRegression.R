91cf354a577de929d017277433f5f2106f26761a
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-01 12:04:33 -0400

    Saving LogisticRegression.R

--- LogisticRegression.R
+++ LogisticRegression.R
@@ -11,5 +11,6 @@ GLM1 <- glm(satlevel ~ Airline.Status +Age+ Gender +Price.Sensitivity+
     Year.of.First.Flight+ No.of.Flights.p.a. +X..of.Flight.with.other.Airlines
     + Type.of.Travel+No..of.other.Loyalty.Cards +Shopping.Amount.at.Airport
     +  Eating.and.Drinking.at.Airport  +  Class Day.of.Month+ Flight.date Airline.Code
-    +Airline.Name+Scheduled.Departure.Hour+ Departure.Delay.in.Minutes+,
+    +Airline.Name+Scheduled.Departure.Hour+ Departure.Delay.in.Minutes+
+    Arrival.Delay.in.Minutes +Flight.cancelled Flight.time.in.minutes,
     data = mydata, family = "binomial")
