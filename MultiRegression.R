2564d1e42455278e40c538ad0ffbb072825b30bf
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-01 12:18:16 -0400

    Saving MultiRegression.R

--- MultiRegression.R
+++ MultiRegression.R
@@ -1,6 +1,18 @@
 
-abc <- data
 
-str(abc)
+dataframe1 <- data
+dataframe1$satisfaction_numeric <- as.numeric(as.character(dataframe1$Satisfaction))
 
+options(scipen = 999)
+predictor_model <- lm(dataframe1$satisfaction_numeric  ~dataframe1$Type.of.Travel + dataframe1$Age + dataframe1$No.of.Flights.p.a., data = dataframe1)
+summary(predictor_model)
+
+plot
+plot(predictor_model)
+abline(predictor_model)
+
+plot.new()
+frame()
+
+str(dataframe1)
 
