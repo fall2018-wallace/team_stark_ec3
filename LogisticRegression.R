1851d09a2944bc8a2602bc8346f391c4f42712c6
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-01 12:06:57 -0400

    Saving LogisticRegression.R

--- LogisticRegression.R
+++ LogisticRegression.R
@@ -10,7 +10,7 @@ satSuv<-satSuv[satSuv$satlevel != 'median',]
 GLM1 <- glm(satlevel ~ Airline.Status +Age+ Gender +Price.Sensitivity+ 
     Year.of.First.Flight+ No.of.Flights.p.a. +X..of.Flight.with.other.Airlines+
      Type.of.Travel+No..of.other.Loyalty.Cards +Shopping.Amount.at.Airport+
-     Eating.and.Drinking.at.Airport  +  Class +Day.of.Month+ Flight.date Airline.Code+
+     Eating.and.Drinking.at.Airport  +  Class +Day.of.Month+ Flight.date +Airline.Code+
     Airline.Name+Scheduled.Departure.Hour+ Departure.Delay.in.Minutes+
     Arrival.Delay.in.Minutes +Flight.cancelled Flight.time.in.minutes+
     Flight.Distance +Arrival.Delay.greater.5.Mins ,
