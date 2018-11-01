720a66b4bdaf2a23e2ad53438851f04b0686749a
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-01 11:28:57 -0400

    Saving Exploring_data.R

--- Exploring_data.R
+++ Exploring_data.R
@@ -1,4 +1,3 @@
 
 d<- clean_data
-f<- lm(formula= as.numeric(d$Satisfaction)~ as.numeric(d$Age) + as.numeric(d$Price.Sensitivity )+ as.numeric(d$No.of.Flights.p.a.) + as.numeric(d$X..of.Flight.with.other.Airlines) + as.numeric(d$No..of.other.Loyalty.Cards)+ as.numeric(d$Shopping.Amount.at.Airport) + as.numeric(d$Day.of.Month) + as.numeric(d$Departure.Delay.in.Minutes) + as.numeric(d$Arrival.Delay.in.Minutes) + as.numeric(d$Flight.time.in.minutes) + as.numeric(d$Flight.Distance), data= d)
-summary(f)
+head(d)
