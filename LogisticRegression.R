50906320bb4b3151aba6a777d015635d1a50a858
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-01 11:57:50 -0400

    Saving LogisticRegression.R

--- LogisticRegression.R
+++ LogisticRegression.R
@@ -6,5 +6,5 @@ satSuv$satlevel[satSuv$Satisfaction >2 & satSuv$Satisfaction <4] <- 'median'
 satSuv$satlevel[satSuv$Satisfaction >3.5] <- 'high'
 head(satSuv)
 
-satSuv[satSuv$satlevel == 'median']
+satSuv[satSuv.satlevel == 'median']
 head(satSuv)
