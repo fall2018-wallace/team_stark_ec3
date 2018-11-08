cca16f9d38effbe760c18cf5378b1cc25f0e24ac
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-08 11:16:40 -0500

    Saving clean.R

--- clean.R
+++ clean.R
@@ -2,7 +2,7 @@
 head(raw_data)
 sum(is.na(raw_data))
 sum(is.null(raw_data))
-clean_data<- na.omit(raw_data,stringsAsFactors = TRUE)
+clean_data<- na.omit(raw_data)
 sum(is.na(clean_data))
 clean_data
 str(clean_data)
