9ecc75a8b6bd992dad4b227bd713460d4c0e7f21
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-08 11:36:34 -0500

    Saving Exploring_data.R

--- Exploring_data.R
+++ Exploring_data.R
@@ -8,6 +8,6 @@ library("ggplot2")
 # using geom_histogram() function with binwidth attribute to group the data 
 # Since the age is below 100  we are using 10 as binwidth 
 my_Plot <- ggplot(clean_data, aes(x=Age))
-my_Plot <- my_Plot + geom_histogram(binwidth = 10)
+my_Plot <- my_Plot + geom_histogram(binwidth = 100)
 my_Plot <- my_Plot + ggtitle("Histogram of age")
 my_Plot
