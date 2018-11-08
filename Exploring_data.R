72c78ff5aa0eff9e24f459e87ce20763a9c631e6
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-08 11:19:02 -0500

    Saving Exploring_data.R

--- Exploring_data.R
+++ Exploring_data.R
@@ -1,3 +1,8 @@
 
 clean_data
 str(clean_data)
+
+#importing library for ggplot2
+library("ggplot2")
+#using ggplot() to generate a histogram 
+# using geom_histogram() function with binwidth attribute to group the data 
