ccdf91909029076b4656f49cd26fedc81bc2fdaa
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-15 11:19:16 -0500

    force updated content when merge failed

--- clean.R
+++ clean.R
@@ -1,9 +1,8 @@
 
-#head(raw_data)
-#str(raw_data)
-#sum(is.na(raw_data))
-#sum(is.null(raw_data))
+head(raw_data)
+sum(is.na(raw_data))
+sum(is.null(raw_data))
 raw_data[is.na(raw_data)]<-0
 clean_data<- raw_data
 sum(is.na(clean_data))
-str(clean_data)
+length(raw_data)
