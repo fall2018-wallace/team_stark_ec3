74b32229238de50e97fcd2f155244f312918ba3e
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-12-03 12:55:20 -0500

    Saving Bayers.R

--- Bayers.R
+++ Bayers.R
@@ -13,16 +13,3 @@ data[data$Satisfaction < 3.5, "Overall_Satisfaction"] <- "Unsatisfied"
 data$Overall_Satisfaction <- as.factor(data$Overall_Satisfaction)
 
 data_clean <- data[,c("Airline.Status", "Gender", "Type.of.Travel", "Class", "Airline.Code", "Airline.Name", "Flight.cancelled", "Arrival.Delay.greater.5.Mins", "Overall_Satisfaction")]
-
-repeating_sequence=rep.int(seq_len(nrow(data_clean)), data_clean$Overall_Satisfaction)
-
-Clean_dataset=data_clean[repeating_sequence,]
-Naive_Bayes_Model=naiveBayes(Overall_Satisfaction ~., data=Clean_dataset)
-Naive_Bayes_Model
-
-
-NB_Predictions=predict(Naive_Bayes_Model,Clean_dataset)
-#Confusion matrix to check accuracy
-table(NB_Predictions,Clean_dataset$Overall_Satisfaction)
-
-
