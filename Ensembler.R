9f92ac7e012fab8a48047d0b976ce1c197b869cf
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-12-06 05:21:31 -0500

    Saving Ensembler.R

--- Ensembler.R
+++ Ensembler.R
@@ -1,6 +1,12 @@
 
 data <- data
-library(tidyverse)
 library(dplyr)
 library(kernlab)
 library(randomForest)
+
+data$Satisfaction <- as.character(data$Satisfaction)
+data[data$Satisfaction >= 3.5, "Overall_Satisfaction"] <- "Satisfied"
+data[data$Satisfaction < 3.5, "Overall_Satisfaction"] <- "Unsatisfied"
+
+data$Overall_Satisfaction <- as.factor(data$Overall_Satisfaction)
+
