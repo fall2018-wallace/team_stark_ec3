014ad1f000bad31667aed307954e9cdce841a95f
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-12-06 05:22:11 -0500

    Saving Ensembler.R

--- Ensembler.R
+++ Ensembler.R
@@ -10,3 +10,24 @@ data[data$Satisfaction < 3.5, "Overall_Satisfaction"] <- "Unsatisfied"
 
 data$Overall_Satisfaction <- as.factor(data$Overall_Satisfaction)
 
+dim(data)
+rand_index <- sample(1:dim(data)[1])
+summary(rand_index)
+length(rand_index)
+
+head(rand_index)
+
+cut_point_2_by_3 <- floor(2 * dim(data)[1] / 3)
+
+trainData <- data[rand_index[1: cut_point_2_by_3],]
+testData <- data[rand_index[(cut_point_2_by_3 + 1) : dim(data)[1]], ]
+outcomeName<-'Overall_Satisfaction'
+
+predictors<-c("Airline.Status", "Age", "Gender", "Type.of.Travel", 
+              "No.of.Flights.p.a.", "Arrival.Delay.greater.5.Mins", "Arrival.Delay.in.Minutes", "Departure.Delay.in.Minutes", 'Flight.time.in.minutes', 'No..of.other.Loyalty.Cards')
+
+fitControl <- trainControl(
+  method = "cv",
+  number = 5,
+  savePredictions = 'final',
+  classProbs = T)
