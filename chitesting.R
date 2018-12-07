724d2834ed83ceaf0764b780273de9e2b1b0333d
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-12-06 21:15:44 -0500

    Saving chitesting.R

--- chitesting.R
+++ chitesting.R
@@ -10,40 +10,42 @@ tb1<-table(satSuv$Airline.Status,satSuv$satlevel)
 tb1
 chisq.test(tb1) 
 # P<0.01,reject the null hypothesis that the satisfaction level is independent 
-#of the airliene status.
+# of the airliene status.
 # They are depended with each other.
 tb2<-table(satSuv$Gender,satSuv$satlevel)
 tb2
 chisq.test(tb2) 
 # P<0.01,reject the null hypothesis that the satisfaction level is independent 
-#of the gender.
+# of the gender.
 # They are depended with each other.
 tb3<-table(satSuv$Type.of.Travel,satSuv$satlevel)
 tb3
 chisq.test(tb3)
 # P<0.01,reject the null hypothesis that the satisfaction level is independent 
-#of the type of travel.
+# of the type of travel.
 # They are depended with each other.
 tb4<-table(satSuv$Class,satSuv$satlevel)
 tb4
 chisq.test(tb4)
 # P<0.01,reject the null hypothesis that the satisfaction level is independent 
-#of the class.
+# of the class.
 # They are depended with each other.
 tb5<-table(satSuv$Airline.Code,satSuv$satlevel)
 tb5
 chisq.test(tb5)
-# P<0.01,reject the null hypothesis that the satisfaction level is independent of the airline code.
+# P<0.01,reject the null hypothesis that the satisfaction level is independent 
+# of the airline code.
 # They are depended with each other.
 tb6<-table(satSuv$Airline.Name,satSuv$satlevel)
 tb6
 chisq.test(tb6)
-# P<0.01,reject the null hypothesis that the satisfaction level is independent of the airline name.
+# P<0.01,reject the null hypothesis that the satisfaction level is independent 
+# of the airline name.
 # They are depended with each other.
 tb7<-table(satSuv$Flight.cancelled,satSuv$satlevel)
 tb7
 chisq.test(tb7)
-# P<0.01,reject the null hypothesis that the satisfaction level is independent of the cancelation of flight.
+# P<0.01,reject the null hypothesis that the satisfaction level is independentof the cancelation of flight.
 # They are depended with each other.
 tb8<-table(satSuv$Arrival.Delay.greater.5.Mins,satSuv$satlevel)
 tb8
